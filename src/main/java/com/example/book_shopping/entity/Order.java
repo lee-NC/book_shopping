c1655d78b859537f950f080b18af66d3072d5a61
svtt-thanhttp <phuongthanh061120@gmail.com>
2022-04-17 16:55:37 +0700

    thanh

--- src/main/java/com/example/book_shopping/entity/Order.java
+++ src/main/java/com/example/book_shopping/entity/Order.java
@@ -3,7 +3,6 @@ package com.example.book_shopping.entity;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
-import java.util.HashSet;
 import java.util.Set;
 
 /**
